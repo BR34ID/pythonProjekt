9feab8c3ec4934f1b43a8d0e34fc0312134347d8
sphrchtr <sophie.richter99@web.de>
2021-12-07 12:43:34 +0100

    Adresseingabe zu Liste, Ausgabe von Text im GUI

--- tsp_gui.py
+++ tsp_gui.py
@@ -1,20 +1,21 @@
 import tkinter as tk
 
+adresslist = [] #um Adressen für Distanzmatrix vorzubereiten
+
 class TSP_GUI:
     def __init__(self,mainW):
         self.lbl1 = tk.Label(mainW, text="Rotenplaner Deluxe", font=("Arial",20))
         self.lbl2 = tk.Label(mainW, text="von Sophie R., Svea-Nele J., Niklas L.", font=("Arial", 14, "italic"))
 
         self.lbl3 = tk.Label(mainW, text="Bitte geben Sie Ihre Zieladressen ein.")
+        self.lbl4 = tk.Label(mainW, text="Bestätigen Sie die Eingabe mit dem Button.")
+
 
         #Entry Felder für Adresseingabe
         self.e1 = tk.Entry(mainW)
-        self.e2 = tk.Entry(mainW)
-        self.e3 = tk.Entry(mainW)
-        self.e4 = tk.Entry(mainW) #wird erst durch Button aktiviert
 
         #Button für mehr Adressfelder
-        self.btnMoreAddresses = tk.Button(mainW, text="Say hello", command=self.moreAddresses)
+        self.btnAddAddress = tk.Button(mainW, text="Adresse hinzufügen.", command=self.addAddress)
 
         #Button für Submit
         self.btnSubmit = tk.Button(mainW, text="Submit!", command=self.submit)
@@ -25,14 +26,17 @@ class TSP_GUI:
         self.lbl2.pack()
         self.lbl3.pack()
         self.e1.pack()
-        self.e2.pack()
-        self.e3.pack()
-        self.btnMoreAddresses.pack()
+        self.btnAddAddress.pack()
         self.btnSubmit.pack()
+        self.lbl4.pack()
 
     #Funktion für mehr Adressfelder (btnMoreAddresses)
-    def moreAddresses(self):
-         self.e4.pack() #dynamisch machen?? 
+    def addAddress(self):
+        self.lbl4.config(text="Adresse hinzugefügt.")
+        #fügt Adresse zu Liste hinzu und Prüft ob Adresse erkannt
+        adresslist.append(self.e1.get())
+        self.e1.delete(0,tk.END)
+        print(adresslist)
 
     def submit(self):
         self.lbl3.config(text="Hallo.")
